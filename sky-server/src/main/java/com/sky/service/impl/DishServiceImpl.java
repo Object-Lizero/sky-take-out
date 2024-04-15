1e96b806500b9b4d3e571ef10c4d04ebd25cb6f7
lizero <1349125253@qq.com>
2024-04-15 17:03:27 +0800

    批量删除菜品业务代码优化

--- sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -105,11 +105,22 @@ public class DishServiceImpl implements DishService {
             throw new DeletionNotAllowedException(MessageConstant.DISH_BE_RELATED_BY_SETMEAL);
         }
         //删除菜品表中的菜品数据
-        for (Long id : ids) {
-            dishMapper.deleteById(id);
-            //删除菜品关联的口味数据
-            dishFlavorMapper.deleteByDishId(id);
-        }
+//        for (Long id : ids) {
+//            dishMapper.deleteById(id);
+//            //删除菜品关联的口味数据
+//            dishFlavorMapper.deleteByDishId(id);
+//        }
+
+        //优化代码后：（减少sql语句数量，提升性能）
+        //根据菜品id集合批量删除菜品数据
+        //sql delete from dish where id in (?,?,?)
+
+        dishMapper.deleteByIds(ids);
+        //根据菜品id集合批量删除口味数据
+        //sql delete from dish_flavor where dishId in (?,?,?)
+        dishFlavorMapper.deleteByDishIds(ids);
+
+
 
 
 
